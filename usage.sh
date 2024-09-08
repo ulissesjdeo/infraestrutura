16a2aef66759547d9d9614e93d4646837f069bcb
Server <server@domain.com>
2024-09-08 19:12:27 -0300

    Improve usage.sh

--- usage.sh
+++ usage.sh
@@ -1,1 +1,13 @@
+echo
+
+docker stats --no-stream --format "table {{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}\t{{.MemPerc}}\t{{.NetIO}}\t{{.BlockIO}}\t{{.PIDs}}"
+echo
+
 docker system df
+echo
+
+du -h -d 1 /infra
+echo
+
+df -h | grep -v '^overlay' | awk 'NR==1 || ($NF!~/^\/var\/lib\/docker/ && $NF!~/^\/var\/lib\/docker\/overlay2/)'
+echo
