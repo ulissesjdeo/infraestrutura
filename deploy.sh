47d761a95c5911eada0e9a7fbd7586713caec7ce
Server <server@domain.com>
2024-09-08 18:42:07 -0300

    Script de deploy

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+sh services/cloudbeaver/install.sh
+sh services/gitlab/install.sh
+sh services/gogs/install.sh
+sh services/mariadb/install.sh
+sh services/portainer/install.sh
+sh services/postgres/install.sh
+sh services/syncthing/install.sh
