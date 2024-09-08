97e0ae1b4c15953594b37eb8233b41e7be39f376
Ulisses João de Oliveira <ulissesjoao2005@gmail.com>
2024-09-08 19:32:36 -0300

    Refactor deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,20 @@
-sh services/papermc/install.sh
+echo 'Tire o comentário dos serviços que irão ser usados'
 
-sh services/portainer/install.sh
-sh services/postgres/install.sh
-sh services/syncthing/install.sh
-sh services/gogs/install.sh
-sh services/gitea/install.sh
-sh services/mariadb/install.sh
-sh services/cloudbeaver/install.sh
-sh services/gitlab/install.sh
+# Banco de dados
+#sh services/cloudbeaver/install.sh
+#sh services/postgres/install.sh
+#sh services/mariadb/install.sh
+
+# Git
+#sh services/gogs/install.sh
+#sh services/gitea/install.sh  # Depende do postgres!
+#sh services/gitlab/install.sh
+
+# Gerenciamento do servidor
+#sh services/portainer/install.sh
+
+# Arquivos
+#sh services/syncthing/install.sh
+
+# Jogos
+#sh services/papermc/install.sh
