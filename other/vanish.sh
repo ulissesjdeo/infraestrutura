517f267785c742bd5929762518b23f8ccb96f8d3
Server <server@domain.com>
2024-09-08 18:45:17 -0300

    Melhorias vanish.sh

--- other/vanish.sh
+++ other/vanish.sh
@@ -1,13 +1,10 @@
-if [[ "$1" == "--yes-i-will-delete-everything" && "$2" == "--i-am-sure" ]]; then
-    echo "Deleting everything..."
-else
-    echo "Error: Required parameters not provided."
-    echo "Usage: $0 --yes-i-will-delete-everything --i-am-sure"
+if [[ "$1" != "--yes-i-will-delete-everything" || "$2" != "--i-am-sure" ]]; then
+    echo "Use: $0 --yes-i-will-delete-everything --i-am-sure"
     exit 1
 fi
 
-docker rm -vf $(docker ps -aq)
+docker rm -vf $(docker ps -aq) 2> /dev/null
 
-docker rmi -f $(docker images -aq)
+docker rmi -f $(docker images -aq) 2> /dev/null
 
 rm -rf /infra
