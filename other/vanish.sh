86f17b45aadc7360c4c0affa9d96d1d6c0fd2b02
Server <server@domain.com>
2024-09-08 18:41:53 -0300

    Seguranca antes de deletar tudo

--- other/vanish.sh
+++ other/vanish.sh
@@ -1,3 +1,11 @@
+if [[ "$1" == "--yes-i-will-delete-everything" && "$2" == "--i-am-sure" ]]; then
+    echo "Deleting everything..."
+else
+    echo "Error: Required parameters not provided."
+    echo "Usage: $0 --yes-i-will-delete-everything --i-am-sure"
+    exit 1
+fi
+
 docker rm -vf $(docker ps -aq)
 
 docker rmi -f $(docker images -aq)
