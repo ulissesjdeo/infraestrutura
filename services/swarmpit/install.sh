5e6401db0aab79cd1abbf8d1dcdc0de2ff71e01f
Ulisses João de Oliveira <ulissesjoao2005@gmail.com>
2024-09-08 16:11:33 -0300

    Padronização swarmpit

--- services/swarmpit/install.sh
+++ services/swarmpit/install.sh
@@ -1,6 +1,12 @@
-# FIXME: Padronizar
+# https://github.com/swarmpit/swarmpit
 
-docker run -it --rm \
-  --name swarmpit-installer \
+docker swarm init
+
+docker run \
+  --rm \
+  -it \
+  \
   --volume /var/run/docker.sock:/var/run/docker.sock \
-swarmpit/install:1.9
+  \
+  --name swarmpit-installer \
+  swarmpit/install:1.9
