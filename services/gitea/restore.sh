87cbddfd63cb475a352961a65d3c0ecdbc3290d1
Server <server@domain.com>
2024-09-10 12:08:39 -0300

    Script de restaurar backup não funciona

--- services/gitea/restore.sh
+++ services/gitea/restore.sh
@@ -18,25 +18,31 @@ fi
 
 # Preparando arquivos
 docker cp "$1" gitea:dump.zip
-docker exec gitea unzip dump.zip
+docker exec gitea sh -c "yes A | unzip dump.zip"
 docker exec gitea rm dump.zip
-docker exec gitea mv $(docker exec gitea ls | grep gitea) DUMP
+docker exec gitea cp -r $(docker exec gitea ls | grep gitea) /DUMP
 
 # Restaurando banco
+docker stop gitea
 until docker exec postgres pg_isready ; do sleep 2 ; done
-docker exec -it postgres psql -U postgres -c "DROP DATABASE gitea; CREATE DATABASE gitea;"
-docker exec -i postgres pg_restore -U postgres -v -d gitea DUMP/PGDUMP.sql
+docker exec -it postgres psql -U postgres -c "DROP DATABASE gitea;"
+docker exec -it postgres psql -U postgres -c "CREATE DATABASE gitea;"
+docker cp gitea:/DUMP/PGDUMP.sql /PGDUMP.sql
+docker cp /PGDUMP.sql postgres:/PGDUMP.sql
+docker exec -i postgres psql -U postgres -v -d gitea -f /PGDUMP.sql
+docker exec postgres rm /PGDUMP.sql
+docker start gitea
 
 # Restaurando data
-docker exec gitea rm -rf data/gitea/*
-docker exec gitea cp -r DUMP/data/* data/gitea/
+docker exec gitea rm -rf /data/gitea/
+docker exec gitea cp -r /DUMP/data/ /data/gitea/
 
 # Restaurando repositórios
-docker exec gitea rm -rf data/git/repositories/
-docker exec gitea cp -r DUMP/repos/* data/git/repositories/
+docker exec gitea rm -rf /data/git/repositories/
+docker exec gitea cp -r /DUMP/repos/ /data/git/repositories/
 
 # Corrigindo permissões
-docker exec gitea chown -R git:git data/
+docker exec gitea chown -R git:git /data/
 
 # Gerando hooks
-docker exec gitea /usr/local/bin/gitea -c '/data/gitea/conf/app.ini'
+docker exec gitea --user git /usr/local/bin/gitea -c '/data/gitea/conf/app.ini'
