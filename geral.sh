ccfc53e8ba42d096cd04dae7920a97763f6af205
Server <server@domain.com>
2024-09-10 10:41:53 -0300

    Update geral.sh

--- geral.sh
+++ geral.sh
@@ -1,3 +1,6 @@
 git config --global init.defaultBranch main
 git config --global user.name 'Server'
 git config --global user.email server@domain.com
+
+git config --global --unset https.proxy
+git config --global --unset http.proxy
