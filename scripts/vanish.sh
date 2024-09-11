892500549297d9fd91bed7b71fc4995d9ddc0c3a
Ulisses João de Oliveira <ulissesjoao2005@gmail.com>
2024-09-11 15:24:21 -0300

    Fix vanish.sh

--- scripts/vanish.sh
+++ scripts/vanish.sh
@@ -1,3 +1,10 @@
-docker rm -vf "$(docker ps -aq)" 2> /dev/null
-docker rmi -f "$(docker images -aq)" 2> /dev/null
+# Remove containers e volumes
+# shellcheck disable=SC2046
+docker rm -vf $(docker ps -a -q) 2> /dev/null
+
+# Remove imagens
+# shellcheck disable=SC2046
+docker image remove -f $(docker images -a -q) 2> /dev/null
+
+# Apaga infraestrutura
 rm -rf /infra
