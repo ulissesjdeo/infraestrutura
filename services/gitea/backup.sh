7c553644acf3ab6f1fdea55f55087d670699d0ef
Ulisses João de Oliveira <ulissesjoao2005@gmail.com>
2024-09-10 23:22:24 -0300

    Backup do gitea

--- services/gitea/backup.sh
+++ services/gitea/backup.sh
@@ -1,7 +1,10 @@
+# Parando script se algo der errado
+set -euo pipefail
+
 # Setup
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
-mkdir /infra/gitea_BACKUP 2> /dev/null
-cd /infra/gitea_BACKUP || exit
+mkdir -p /BACKUP/gitea 2> /dev/null
+cd /BACKUP/gitea || exit
 
 # Dump dos dados
 docker exec -u 1000 -it -w /tmp gitea bash -c '/usr/local/bin/gitea dump -c /data/gitea/conf/app.ini'
@@ -11,28 +14,32 @@ BACKUP_FILE=$(docker exec gitea bash -c "ls -t /tmp/gitea-dump-*.zip | head -1")
 FILENAME=$(basename "$BACKUP_FILE")
 
 # Copiando dump para host
-docker cp gitea:"$BACKUP_FILE" /infra/gitea_BACKUP
+docker cp gitea:"$BACKUP_FILE" /BACKUP/gitea/
 
 # Abrindo backup
-unzip "$FILENAME" -d gitea
+unzip -q "$FILENAME" -d gitea-dump 1> /dev/null
 
-# Apagando zip original
-rm "$FILENAME"
-
-# Remove backup do XORM pois ele é potencialmente problemático
-rm gitea/gitea-db.sql
+# Parando Gitea
+docker stop gitea
 
 # Aguardando postgres ficar pronto
-until docker exec postgres pg_isready ; do sleep 2 ; done
+until docker exec "$POSTGRES_CONTAINER" pg_isready -q; do
+    echo "PostgreSQL is unavailable - sleeping"
+    sleep 2
+done
 
 # pg_dump do gitea
-docker exec postgres pg_dump -U postgres gitea > gitea/PGDUMP.sql
+rm gitea-dump/gitea-db.sql
+docker exec postgres pg_dump -U postgres -Fc -d gitea > gitea-dump/gitea-db.dump
 
-# Salvando com timestamp
-mv gitea/ "$TIMESTAMP"_gitea/
+# Iniciando Gitea
+docker start gitea
 
-# Empacotando
-zip -r "$TIMESTAMP"_gitea.zip "$TIMESTAMP"_gitea/
+# Limpa arquivos desnecessários
+rm gitea-dump/app.ini
 
-# Removendo pasta
-rm -r "$TIMESTAMP"_gitea/
+# Compacta
+zip -r gitea-dump-"$TIMESTAMP".zip gitea-dump/
+
+# Limpa
+rm -rf gitea-dump/ "$FILENAME"
