06d9e73c4bba8866036b3ddec64b6bcda089b049
Ulisses João de Oliveira <ulissesjoao2005@gmail.com>
2024-09-11 00:32:48 -0300

    Simplify vanish.sh

--- scripts/vanish.sh
+++ scripts/vanish.sh
@@ -1,8 +1,3 @@
-if [[ "$1" != "--yes-i-will-delete-everything" || "$2" != "--i-am-sure" ]]; then
-    echo "Use: $0 --yes-i-will-delete-everything --i-am-sure"
-    exit 1
-fi
-
 docker rm -vf "$(docker ps -aq)" 2> /dev/null
 docker rmi -f "$(docker images -aq)" 2> /dev/null
 rm -rf /infra
