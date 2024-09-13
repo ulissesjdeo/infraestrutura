bff735131914fb16a3594c136e35d3147d17db22
Ulisses João de Oliveira <ulissesjoao2005@gmail.com>
2024-09-12 21:21:51 -0300

    Refactoring gitea/backup.sh

--- services/gitea/backup.sh
+++ services/gitea/backup.sh
@@ -1,41 +1,35 @@
-# Setup
+echo "=== INICIANDO BACKUP"
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+
+echo "=== CRIANDO PASTAS"
 mkdir -p /BACKUP/gitea 2> /dev/null
 cd /BACKUP/gitea || exit
 
-# Dump dos dados
-docker exec -u 1000 -it -w /tmp gitea bash -c '/usr/local/bin/gitea dump --skip-log --skip-db -c /data/gitea/conf/app.ini'
+echo "=== FAZENDO DUMP (SEM BANCO/LOGS)"
+docker exec -u 1000 -it -w /tmp gitea bash -c '/usr/local/bin/gitea dump --skip-log --skip-db --quiet -c /data/gitea/conf/app.ini'
 
-# Path do dump no container
-BACKUP_FILE=$(docker exec gitea bash -c "ls -t /tmp/gitea-dump-*.zip | head -1")
-FILENAME=$(basename "$BACKUP_FILE")
-
-# Copiando dump para host
-docker cp gitea:"$BACKUP_FILE" /BACKUP/gitea/
-
-# Abrindo backup
-unzip -q "$FILENAME" -d gitea-dump
-
-# Parando Gitea
+echo "=== PARANDO GITEA"
 docker stop gitea
 
-# Aguardando postgres ficar pronto
+echo "=== COPIANDO DUMP"
+BACKUP_FILE=$(docker exec gitea bash -c "ls -t /tmp/gitea-dump-*.zip | head -1")
+FILENAME=$(basename "$BACKUP_FILE")
+docker cp gitea:"$BACKUP_FILE" /BACKUP/gitea/
+
+echo "=== DESCOMPACTANDO DUMP"
+unzip -q "$FILENAME" -d gitea-dump
+
 until docker exec postgres pg_isready -q; do
-    echo "PostgreSQL is unavailable - sleeping"
-    sleep 2
+    echo "=== AGUARDANDO POSTGRES"; sleep 2
 done
 
-# pg_dump do gitea
+echo "=== FAZENDO DUMP DO BANCO"
 docker exec postgres pg_dump -U postgres -Fc -d gitea > gitea-dump/gitea-db.dump
 
-# Iniciando Gitea
+echo "=== INICIANDO GITEA"
 docker start gitea
 
-# Limpa arquivos desnecessários
+echo "=== LIMPANDO E COMPACTANDO"
 rm gitea-dump/app.ini
-
-# Compacta
 zip -r gitea-dump-"$TIMESTAMP".zip gitea-dump/
-
-# Limpa
 rm -rf gitea-dump/ "$FILENAME"
